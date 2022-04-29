1e039220d71c03e86fd4439b98429e691219b5b8
TymekB <baran.tymoteusz@gmail.com>
2022-04-29 20:28:01 +0200

    Add User and users list

--- lab06/Program.cs
+++ lab06/Program.cs
@@ -6,7 +6,13 @@ namespace lab06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<User> users = new List<User>()
+            {
+                new User { Name = "A", Role = "STUDENT", Marks = new int[]{ 5,1,4 }},
+                new User { Name = "B", Role = "STUDENT", Marks = new int[]{ 0,1,4 }},
+                new User { Name = "C", Role = "ADMIN"},
+                new User { Name = "A", Role = "TEACHER"}
+            };
         }
     }
 }
