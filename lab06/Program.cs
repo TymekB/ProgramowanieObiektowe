3c1f520f578680d7ba0aef667fbc0bce770c51ef
TymekB <baran.tymoteusz@gmail.com>
2022-05-01 20:02:18 +0200

    Add solution to task 1

--- lab06/Program.cs
+++ lab06/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lab06
 {
     class Program
     {
+        static void zad1(List<User> users)
+        {
+            Console.WriteLine("ZAD 1 - Ilosc rekordow w tablicy");
+
+            int result = users.Count();
+            // int result = (from user in users select user).Count();
+            
+            Console.WriteLine(result + "\n");
+        }
+
         static void Main(string[] args)
         {
             List<User> users = new List<User>()
@@ -13,6 +25,7 @@ namespace lab06
                 new User { Name = "C", Role = "ADMIN"},
                 new User { Name = "A", Role = "TEACHER"}
             };
+            zad1(users);
         }
     }
 }
