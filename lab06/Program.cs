daec137112fb3a2b18d5583d11731d34cff26bc9
TymekB <baran.tymoteusz@gmail.com>
2022-05-01 20:17:59 +0200

    Add solution to task 3

--- lab06/Program.cs
+++ lab06/Program.cs
@@ -30,6 +30,17 @@ namespace lab06
 
         }
 
+        static void zad3(List<User> users)
+        {
+            Console.WriteLine("ZAD 3 - Posortowani uzytkownicy po nazwach");
+            
+            List<User> result = users.OrderBy(user => user.Name).ToList();
+            // List<User> result = (from user in users orderby user.Name select user).ToList();
+
+            foreach (User user in result)
+                Console.WriteLine(user.Name);
+        }
+
         static void Main(string[] args)
         {
             List<User> users = new List<User>()
@@ -37,10 +48,11 @@ namespace lab06
                 new User { Name = "A", Role = "STUDENT", Marks = new int[]{ 5,1,4 }},
                 new User { Name = "B", Role = "STUDENT", Marks = new int[]{ 0,1,4 }},
                 new User { Name = "C", Role = "ADMIN"},
-                new User { Name = "A", Role = "TEACHER"}
+                new User { Name = "D", Role = "TEACHER"}
             };
             zad1(users);
             zad2(users);
+            zad3(users);
         }
     }
 }
