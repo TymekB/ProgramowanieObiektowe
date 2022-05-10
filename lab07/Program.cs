fcafc3970f4caa559453f18e0ec92c07e9a4c315
TymekB <baran.tymoteusz@gmail.com>
2022-05-10 21:20:47 +0200

    lab07 init

---
+++ lab07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Linq;
+using System.Linq;
+
+namespace lab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uwaga: zmień `DESKTOP-123ABC\SQLEXPRESS` na nazwę swojego serwera.
+
+            string connectionString =
+                @"Data Source=DESKTOP-F6VNSC1;Initial Catalog=projectdb;Integrated Security=True";
+
+            using (DataContext dataContext = new DataContext(connectionString))
+            {
+                Table<UserEntity> users = dataContext.GetTable<UserEntity>();
+
+                IQueryable<UserEntity> query = from user in users
+                    where user.RemovedAt.HasValue == false // user.RemovedAt == null
+                    select user;
+
+                foreach (UserEntity user in query)
+                    Console.WriteLine("{0} {1}", user.Id, user.Name);
+            }
+        }
+    }
+}
---
+++ lab07/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace lab07
+{
+    [Table(Name = "User")]
+    public class UserEntity
+    {
+        [Column(IsPrimaryKey = true, Name = "Id")]
+        public long Id { get; set; }
+
+        [Column(Name = "Name")] public string Name { get; set; }
+
+        [Column(Name = "Role")] public string Role { get; set; } // ADMIN, MODERATOR, TEACHER, STUDENT
+
+        [Column(Name = "CreatedAt")] public DateTime? CreatedAt { get; set; }
+
+        [Column(Name = "RemovedAt")] public DateTime? RemovedAt { get; set; }
+    }
+}
